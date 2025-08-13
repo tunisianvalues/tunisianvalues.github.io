e2251c7f0b91a49cd2ebce5239ba06f52dc44df9
Rakez <azonitefr@gmail.com>
2025-08-13 16:10:20 +0100

    Add files via upload

---
+++ en/questions.js
@@ -0,0 +1,1002 @@
+questions = [
+  {
+    question:
+      "Tunisia should prioritize alliances with Arab countries over other regions.",
+    effect: {
+      pana: +4,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "The political and economic unity of the Arab world is a priority objective.",
+    effect: {
+      pana: +8,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Artistic and cultural competitions on the scale of the Arab world should be developed.",
+    effect: {
+      pana: +4,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Tunisians primarily share an Arab identity.",
+    effect: {
+      pana: +4,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "A common Arab army would be beneficial to Tunisia's security.",
+    effect: {
+      pana: +8,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisia should automatically support its Arab neighbors in case of conflict with a non-Arab country.",
+    effect: {
+      pana: +8,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Tunisia is more Arab than African.",
+    effect: {
+      pana: +6,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisia must above all defend its national interests, even against Arab countries.",
+    effect: {
+      pana: -8,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisian policies should be designed independently of the priorities of the Arab world.",
+    effect: {
+      pana: -8,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisian culture is distinct and must be protected from external influences, including from the Arab world.",
+    effect: {
+      pana: -6,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisia's diplomatic decisions should not be influenced by Arab causes if they do not directly concern it.",
+    effect: {
+      pana: -6,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "The Arab world does not exist.",
+    effect: {
+      pana: -8,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisia must actively participate in international organizations.",
+    effect: {
+      pana: 0,
+      coop: +4,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "International treaties are necessary to protect Tunisian interests.",
+    effect: {
+      pana: 0,
+      coop: +4,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisia should strengthen its partnerships with powers like the EU, the United States, or China.",
+    effect: {
+      pana: 0,
+      coop: +6,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Opening up to foreign investment is essential for the country's development.",
+    effect: {
+      pana: 0,
+      coop: +4,
+      econ: -4,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisia should engage in humanitarian and peacekeeping missions abroad.",
+    effect: {
+      pana: 0,
+      coop: +6,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisia must remain in control of its political choices, even if it means facing international isolation.",
+    effect: {
+      pana: 0,
+      coop: -4,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "International institutions do not respect Tunisia's sovereignty and do not protect its fundamental interests.",
+    effect: {
+      pana: 0,
+      coop: -8,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisia should reduce its dependence on foreign aid and partnerships.",
+    effect: {
+      pana: 0,
+      coop: -4,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Tunisia would benefit from following the Bourguibist doctrine of neutrality and non-interference in external conflicts.",
+    effect: {
+      pana: 0,
+      coop: -4,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "The Tunisian state must control strategic sectors of the economy such as energy, transport, and telecommunications.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: +8,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Essential public services like health and education must remain free and accessible to all, even if it requires a tax increase.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: +4,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Private companies must be abolished.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: +8,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Large private companies must be regulated to avoid abuses.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: +6,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "The redistribution of wealth is essential to reduce inequalities.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: +6,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Free competition stimulates innovation and economic growth.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: -4,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Taxes on businesses should be reduced to encourage investment.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: -6,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "The private sector is more efficient than the state in economic management.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: -8,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "The privatization of certain public companies would be beneficial.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: -4,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Inequalities are inevitable in a prosperous economy.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: -8,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "The state must massively subsidize bread, gasoline, and electricity.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: +6,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "The Tunisian state should carry out an exceptional recruitment of unemployed PhD holders to solve the employment crisis in the higher education sector.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: +6,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "The Tunisian economic model could work, but it is held hostage by a corrupt elite that enriches itself while the people become poorer.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: +8,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Sharia should inspire Tunisian legislation.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: +8,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Religion must play a central role in political life.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: +6,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Islamic values are essential to guide society.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: +6,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "The state must actively support religious institutions.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: +4,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Islamist political parties must be allowed.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: +6,
+      soci: 0,
+      demo: +2,
+      decent: 0,
+    },
+  },
+  {
+    question: "Religion and politics must be completely separate.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: -8,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Laws must be based solely on civil principles, not religious ones.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: -8,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Public institutions must be neutral towards all beliefs.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: -6,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Freedom of worship must include the freedom not to believe.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: -4,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Religious discourse should not influence political decisions.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: -4,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "All mention of Islam must be removed from the constitution.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: -8,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "The Tunisian education system must align more with the principles of Islam, the moral and identity foundation of the individual.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: +6,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "My religious identity takes precedence over my national affiliation; I am a Muslim before being Tunisian.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: +4,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Traditional Tunisian values and customs must be preserved.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: +4,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Rapid social reforms can destabilize society.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: +4,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "The traditional role of the family must be protected.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: +6,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Ancient cultural norms still have their place today.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: +6,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Total equality between men and women must be guaranteed by law.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: -6,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Sexual minorities must have the same rights as other citizens.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: -8,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Laws must evolve with mindsets.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: -6,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Traditions that limit individual freedoms must be abolished.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: -8,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Cultural innovation is more important than preserving customs.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: -6,
+      demo: 0,
+      decent: 0,
+    },
+  },
+  {
+    question: "Free and regular elections are essential.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: +6,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Checks and balances (justice, parliament, media) must be protected.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: +6,
+      decent: 0,
+    },
+  },
+  {
+    question: "Peaceful demonstrations are a fundamental right.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: +4,
+      decent: 0,
+    },
+  },
+  {
+    question: "Political pluralism is essential for democracy.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: +6,
+      decent: 0,
+    },
+  },
+  {
+    question: "Government transparency is a priority.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: +6,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "A strong leader is sometimes necessary, even at the expense of political freedoms.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: -6,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "In certain situations, it is acceptable to limit freedom of the press.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: -6,
+      decent: 0,
+    },
+  },
+  {
+    question: "Political opponents threaten the country's stability.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: -8,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "An authoritarian government is more effective than a liberal democracy.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: -8,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Important decisions must be made quickly, even without public debate.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: -4,
+      decent: 0,
+    },
+  },
+  {
+    question:
+      "Local authorities better understand the needs of their population.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: +6,
+    },
+  },
+  {
+    question: "Governors should be elected locally.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: +8,
+    },
+  },
+  {
+    question:
+      "A strong central power is necessary to avoid regional divisions.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: -4,
+    },
+  },
+  {
+    question:
+      "Economic decisions must remain in the hands of the central government.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: -6,
+    },
+  },
+  {
+    question:
+      "The management of natural resources must remain under the control of the central government.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: -6,
+    },
+  },
+  {
+    question: "Too much local power can weaken national unity.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: -6,
+    },
+  },
+  {
+    question: "The same laws must apply uniformly throughout the territory.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: -6,
+    },
+  },
+  {
+    question:
+      "Post-revolution decentralization is essential to improve governance and bring decisions closer to the citizens.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: +4,
+      decent: -6,
+    },
+  },
+
+  // SPECIAL QUESTIONS
+  {
+    question:
+      "It would be better for Tunisia to restore a constitutional monarchy.",
+    effect: { pana: 0, coop: 0, econ: 0, reli: 0, soci: 0, demo: 0, decent: 0 },
+    special: "monarchie",
+  },
+  {
+    question:
+      "The UGTT (Tunisian General Labour Union) has a positive role in Tunisian political and social life.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: +2,
+      reli: 0,
+      soci: 0,
+      demo: +2,
+      decent: 0,
+    },
+    special: "ugtt",
+  },
+  {
+    question:
+      "The measures taken by Kaïs Saïed on July 25, 2021, were necessary and justified to save the country.",
+    effect: { pana: 0, coop: 0, econ: 0, reli: 0, soci: 0, demo: 0, decent: 0 },
+    special: "25juillet",
+  },
+  {
+    question: "Kaïs Saïed is a good president.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: -4,
+      decent: 0,
+    },
+    special: "ks",
+  },
+  {
+    question: "Tunisia should have avoided the revolutionary process of 2011.",
+    effect: {
+      pana: 0,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: -4,
+      decent: 0,
+    },
+    special: "2011",
+  },
+  {
+    question:
+      "The future of Tunisia lies in a united Maghreb, founded on our historical and cultural ties.",
+    effect: { pana: 0, coop: 0, econ: 0, reli: 0, soci: 0, demo: 0, decent: 0 },
+    special: "maghreb",
+  },
+  {
+    question:
+      "Tunisia is part of Africa and must take a leading role in building a strong and sovereign African union.",
+    effect: {
+      pana: -2,
+      coop: 0,
+      econ: 0,
+      reli: 0,
+      soci: 0,
+      demo: 0,
+      decent: 0,
+    },
+    special: "afrique",
+  },
+];
